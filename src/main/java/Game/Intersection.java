24e1f2ff0b3e7e458b8cc0f039b1de6c19e63378
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-22 12:52:45 +0100

    Removed update function, intersections are never updated, new position with new intersections is constructed.

--- src/main/java/Game/Intersection.java
+++ src/main/java/Game/Intersection.java
@@ -1,6 +1,7 @@
 package Game;
 
 public class Intersection {
+
   public Stone stone;
   public int index;
 
@@ -8,12 +9,6 @@ public class Intersection {
     this.index = index;
     stone = Stone.None;
   }
-
-  /**
-   * Update what stone, if any, is currently located on this intersection.
-   * @param stone that is now in the intersection.
-   */
-  public void updateIntersection(Stone stone) {
-    this.stone = stone;
-  }
 }
+
+
