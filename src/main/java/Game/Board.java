261070e4da6d243806a16cc70bb6f88b1e1b769c
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-22 10:43:39 +0100

    Added constructor that constructs an empty board with configurable size, included methods to update and return the board position

--- src/main/java/Game/Board.java
+++ src/main/java/Game/Board.java
@@ -1,8 +1,36 @@
 package Game;
 
+import java.util.List;
+
 /**
  * Class that represents the board where the game is played.
  */
 public class Board {
+  public Integer dimension;
+  public Position currentPosition;
+  private List<Position> previousPositions;
 
+  /**
+   * Create a new board.
+   * @param dimension of the board, boards are square
+   */
+  public Board(int dimension) {
+    this.dimension = dimension;
+    // Create the first position using the constructor that requires the dimension of the board
+    currentPosition = new Position(dimension);
+  }
+
+  /**
+   * Update the current position of the board with the new position.
+   * @param newPosition on the board
+   */
+  public void updatePosition(Position newPosition) {
+    currentPosition = newPosition;
+  }
+
+  /**
+   * return the current position.
+   * @return currentPosition
+   */
+  public Position getCurrentPosition() { return currentPosition; }
 }
