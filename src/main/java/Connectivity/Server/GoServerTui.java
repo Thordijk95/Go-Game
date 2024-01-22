79a5e8b37deca80594add9cf82a8f43da99d0df8
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-22 12:49:37 +0100

    Further implemented the server, added a tui to control the server and adjust settings

---
+++ src/main/java/Connectivity/Server/GoServerTui.java
@@ -0,0 +1,53 @@
+package Connectivity.Server;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class GoServerTui {
+  private Integer dimension = 9;
+  private GoServer goServer;
+
+  public GoServerTui() {}
+
+  public static void main(String[] args) {
+    GoServerTui serverTui = new GoServerTui();
+    serverTui.run();
+  }
+
+  public void run() {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please provide the port where to start the server:");
+    int port = input.nextInt();
+    try {
+      goServer = new GoServer(port);
+      System.out.println("Server opened port " + port);
+      goServer.acceptConnections();     // Start accepting incoming connections
+    } catch (IOException e) {
+      System.out.println("Starting the server failed");
+      e.printStackTrace();
+    }
+    help();
+    while (!input.nextLine().equals("--quit")) {
+      String[] splitString = input.nextLine().replace(" ", "").split("~");
+      switch (splitString[0]) {
+        case "--dimension":
+          dimension =Integer.parseInt(splitString[1]);
+        case "--help":
+          help();
+        default:
+          System.out.println("That is not a valid input.");
+          help();
+      }
+      System.out.println("Provide the next input");
+    }
+  }
+
+  public void help() {
+    System.out.println("This is the help of the GoServerTui");
+    System.out.println("--help provides the user with this menu");
+    System.out.println("The standard dimensions of Go Games being started is 9x9");
+    System.out.println("--dimension ~ <N> allows the user to set the dimensions of new Go Games.");
+    System.out.println("--quit stops the server");
+  }
+
+}
