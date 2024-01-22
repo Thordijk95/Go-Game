5ed93a856f02f6196e77c600a6af1fc1bce8424d
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-22 15:08:18 +0100

    Added methods to the interface

--- src/main/java/Game/Player/Player.java
+++ src/main/java/Game/Player/Player.java
@@ -1,8 +1,9 @@
 package Game.Player;
 
 public interface Player {
-
   String getUsername();
-
+  void setUsername(String username);
+  void sendMessage(String message);
+  void sendMove(String message);
 
 }
