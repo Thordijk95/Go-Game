c0184b5d71630ec0d959196ff3caf372f94f1be6
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-22 10:40:29 +0100

    Class that can store an X and Y coordinate

---
+++ src/main/java/Game/IntersectionCoordinate.java
@@ -0,0 +1,15 @@
+package Game;
+
+final class IntersectionCoordinate {
+  private Integer x;
+  private Integer y;
+
+  public IntersectionCoordinate(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public Integer getX() { return x; }
+  public Integer getY() { return y; }
+
+}
