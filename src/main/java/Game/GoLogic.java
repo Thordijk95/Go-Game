ff5b754825300b6f2e191592f767db0d92ab4699
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-22 12:51:47 +0100

    Implemeneted small logic to convert an index to coordinates and store this in and IntersectionCoordinate class

--- src/main/java/Game/GoLogic.java
+++ src/main/java/Game/GoLogic.java
@@ -22,18 +22,34 @@ public class GoLogic {
   }
 
   /**
-   * Calcualte the score of the current position.
+   * Calculate the score of the current position.
    * @param position that we want to know the score of
    * @return the score of the position
    */
   public int score(Position position) {
     // Calculate the score of both players in the current position
-    return 0; //TODO
+    return 0; //TODO return the actual score of the position
   }
 
   public Player getWinner(Game game) {
     // returns the winner of the board connected to the game logic
     return (Player) new PlayerPlayer(); // TODO return the proper player
 
+  }
+
+  /**
+   * Calculate the X and Y coordinates of an intersection.
+   * @param index of the intersection of which the x and y need to be known
+   * @param dimension of the board, this is not stored in the logic
+   * @return an object of the class IntersectionCoordinate that contains and X and Y field
+   */
+  public IntersectionCoordinate calculateXY(int index, int dimension) {
+    if (index <= dimension) {
+      return new IntersectionCoordinate(index, 0);
+    } else {
+      int y = index / dimension;
+      int x = index % dimension;
+      return new IntersectionCoordinate(x,y);
+    }
   }
 }
