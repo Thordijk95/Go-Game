38ab85118ad03f7c27ce2012917c0fd280c6f305
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-22 15:09:42 +0100

    Implemented abstract methods

--- src/main/java/Game/Player/PlayerPlayer.java
+++ src/main/java/Game/Player/PlayerPlayer.java
@@ -1,6 +1,36 @@
 package Game.Player;
 
-public class PlayerPlayer {
+import Connectivity.Client.PlayerConnection;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-  public PlayerPlayer() {}
+public class PlayerPlayer extends Game.Player.AbstractPlayer {
+  private PlayerConnection playerConnection;
+  private String username;
+
+  public PlayerPlayer() { }
+
+  public PlayerPlayer(String username) {
+    this.username = username;
+  }
+
+  public void setPlayerConnection(InetAddress inetAddress, int port) throws IOException {
+    try {
+      playerConnection = new PlayerConnection(InetAddress.getLocalHost(), port);
+      playerConnection.player = this;
+    } catch (UnknownHostException e){
+      e.printStackTrace();
+    }
+  }
+  @Override
+  public void sendMessage(String message) {
+    playerConnection.sendMessage(message);
+  }
+
+  @Override
+  public void sendMove(String message) {
+
+  }
 }
