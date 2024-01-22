82a94a8777453eff1a7bb2b4f90e63f8ab6047bd
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-22 10:40:05 +0100

    Setup for the protocol to use, central spot for communication and for easy changes

--- src/main/java/Connectivity/Protocol/GoProtocol.java
+++ src/main/java/Connectivity/Protocol/GoProtocol.java
@@ -1,4 +1,17 @@
 package Connectivity.Protocol;
 
-public enum GoProtocol {
+public final class GoProtocol {
+  public static final String SEPARATOR = "~";
+  public static final String LOGIN = "LOGIN";
+  public static final String QUEUE = "QUEUE";
+  public static final String MOVE = "MOVE";
+  public static final String ERROR = "ERROR";
+  public static final String HELLO = "HELLO";
+  public static final String ACCEPTED = "ACCEPTED";
+  public static final String REJECTED = "REJECTED";
+  public static final String QUEUED = "QUEUED";
+  public static final String GAMESTARTED = "GAME STARTED";
+  public static final String DISCONNECTED = "DISCONNECTED";
+  private GoProtocol() {}
+
 }
