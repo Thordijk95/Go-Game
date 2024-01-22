0c54b9604c79c2667d944b68ec85cc72373af376
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-22 12:51:09 +0100

    Implemneted updateBoard method and renamed field

--- src/main/java/Game/GoGame.java
+++ src/main/java/Game/GoGame.java
@@ -16,13 +16,13 @@ public class GoGame implements Game{
   /**
    * When a game is started the logic is added and the board is created.
    */
-  public GoGame(){
+  public GoGame(int dimension){
     logic = new GoLogic();
-    board = new Board();
+    board = new Board(dimension);
   }
 
   // List of old positions
-  private List<Position> oldPositions = new LinkedList();
+  private List<Position> previousPositions = new ArrayList<>();
   // Hashmap that links the ith positions to the score
   private HashMap<Integer, Integer> scores = new HashMap<>();
 
@@ -34,9 +34,13 @@ public class GoGame implements Game{
 
   }
 
-  public void updatePositions(Position newPosition, int score) {
-    oldPositions.add(newPosition);
-    logic.score(newPosition);
+  public void updatePositions(Position newPosition) {
+    // Update the history and the HashMap used for comparing positions
+    previousPositions.add(newPosition);
+    int score = logic.score(newPosition);
+    scores.put(previousPositions.size()+1, score);
+    // Update the board
+    board.updatePosition(newPosition);
   }
 
 
