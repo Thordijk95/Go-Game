7f51e0804a1bff77cac158a2b1ae6775b89f3ced
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-22 15:10:07 +0100

    Override methods from abstract'

--- src/main/java/Game/Player/AIPlayer.java
+++ src/main/java/Game/Player/AIPlayer.java
@@ -1,5 +1,19 @@
 package Game.Player;
 
-public class AIPlayer {
+public class AIPlayer extends AbstractPlayer{
+  private static final String username = "AI";
 
+  public AIPlayer(String username) {
+    super(username);
+  }
+
+  @Override
+  public void sendMessage(String message) {
+
+  }
+
+  @Override
+  public void sendMove(String message) {
+
+  }
 }
