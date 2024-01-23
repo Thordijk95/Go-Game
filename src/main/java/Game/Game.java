ab6217d743d25b2598b87a056a2ced680ec0ecc7
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-23 12:26:14 +0100

    Updated interface with more methods

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,5 +1,22 @@
 package Game;
 
-public interface Game {
+import Connectivity.Server.ConnectionHandler;
+import Game.Player.Player;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import javafx.geometry.Pos;
+
+public interface Game{
+  List<ConnectionHandler> players = new ArrayList<>();
+
+  String getStateString();
+  Position getStatePosition();
+  boolean validateMove(Move move, ConnectionHandler player);
+
+  void updateState();
+
+  HashMap<Stone, ConnectionHandler> getPLayers();
+  void switchTurn();
 
 }
