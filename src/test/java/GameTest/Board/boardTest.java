d82004aad09f3d48d403beb68bfad130fcf0c4d6
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-23 12:31:00 +0100

    Implemented first test to test the creation of a board and verify that the dimensions are correct

--- src/test/java/GameTest/Board/boardTest.java
+++ src/test/java/GameTest/Board/boardTest.java
@@ -1,5 +1,27 @@
 package GameTest.Board;
 
-public class boardTest {
+import static org.junit.Assert.assertEquals;
 
+import Game.Board;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class boardTest {
+  private Board testBoard;
+  private static final int boardDimension = 9;
+  @BeforeEach
+  void setup() {
+    testBoard = new Board(boardDimension);
+  }
+
+  @Test
+  void setBoardDimension() {
+    assertEquals(testBoard.dimension, boardDimension);
+  }
+
+  @Test
+  void updateBoard() {
+
+  }
 }
