552bfe0744c51a3f444e3876899f76e5caa3ce67
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-24 11:37:52 +0100

    removed getX, getY methods

--- src/main/java/Game/IntersectionCoordinate.java
+++ src/main/java/Game/IntersectionCoordinate.java
@@ -1,15 +1,12 @@
 package Game;
 
-final class IntersectionCoordinate {
+public class IntersectionCoordinate {
   private Integer x;
   private Integer y;
 
-  public IntersectionCoordinate(int x, int y) {
+  public IntersectionCoordinate(int x, int y){
     this.x = x;
     this.y = y;
-  }
-
-  public Integer getX() { return x; }
-  public Integer getY() { return y; }
+  };
 
 }
