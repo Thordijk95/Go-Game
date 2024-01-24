b55d711859354c2b184db8ba664bb698e875b49d
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-24 11:28:33 +0100

    Extended test method for clusters, and implementation of findNeighbors in GoLogic.  Test now creates 4 clusters, 3 of which have the same border length as stones in the cluster, 1 has an enclosed stone. Test is passed

--- src/main/java/Game/GoLogic.java
+++ src/main/java/Game/GoLogic.java
@@ -2,7 +2,10 @@ package Game;
 
 import Game.Player.Player;
 import Game.Player.PlayerPlayer;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import javafx.util.Pair;
 
 public class GoLogic {
 
@@ -23,13 +26,16 @@ public class GoLogic {
    * @param dimension of the board, this is not stored in the logic
    * @return an object of the class IntersectionCoordinate that contains and X and Y field
    */
-  public IntersectionCoordinate calculateXY(int index, int dimension) {
-    if (index <= dimension) {
-      return new IntersectionCoordinate(index, 0);
+  public int[] calculateXY(int index, int dimension) {
+    int[] coordinates = new int[2];
+    if (index < dimension) {
+      coordinates[0] = index;
+      coordinates[1] = 0;
+      return coordinates;
     } else {
-      int y = index / dimension;
-      int x = index % dimension;
-      return new IntersectionCoordinate(x,y);
+      coordinates[0] = index % dimension;
+      coordinates[1] = index / dimension;
+      return coordinates;
     }
   }
 
@@ -57,5 +63,149 @@ public class GoLogic {
       }
     }
     return true;
+  }
+
+  /**
+   * Find all the clusters of stones, cluster is any group of neighboring stones of the same color.
+   * @param position of the board.
+   * @param dimension of the board.
+   * @return a list of clusters
+   */
+  public List<Cluster> stoneClusters(Position position, int dimension) {
+    List<Cluster> clusters = new ArrayList<>();
+    for (int i = 0; i < dimension*dimension; i++) {
+      int[] coordinate = calculateXY(i, dimension);
+      List<int[]> neighborsStraight = findNeighborsStraigh(coordinate, dimension);
+      List<int[]> neighbors = findNeighborsDiagnoal(neighborsStraight, coordinate, dimension);
+      boolean found = false;
+      // Check if the current intersection is taken by one of the players
+      if (position.getIntersection(i).stone != Stone.NONE) {
+        if (clusters.isEmpty()) { // first cluster found
+          Cluster cluster = new Cluster(position.getIntersection(i).stone);
+          cluster.intersectionList.add(coordinate);
+          defineBorder(position, dimension, neighborsStraight, cluster, coordinate);
+          clusters.add(cluster);
+        } else {
+          // Check if the current intersection is connected to one of the existing clusters and the same stone
+          for (int[] neighbor : neighbors) {
+            for (Cluster cluster : clusters) {
+              for (int[] intersection : cluster.intersectionList) {
+                if (Arrays.equals(intersection, neighbor) && position.getIntersection(i).stone == cluster.stone) {
+                  cluster.intersectionList.add(coordinate);
+                  defineBorder(position, dimension, neighborsStraight, cluster, coordinate);
+                  found = true;
+                  break;
+                } else if (Arrays.equals(intersection, neighbor) && position.getIntersection(i).stone != cluster.stone) {
+                  boolean front = true;
+                }
+              }
+            }
+            if (found) {break;}
+          }
+          // All neighbors are part of a different cluster or no cluster, make a new cluster
+          if (!found) {
+            Cluster cluster = new Cluster(position.getIntersection(i).stone);
+            cluster.intersectionList.add(coordinate);
+            defineBorder(position, dimension, neighborsStraight, cluster, coordinate);
+            clusters.add(cluster);
+          }
+        }
+      }
+    }
+
+    return clusters;
+  }
+
+  /**
+   * Method that defines the border of a cluster.
+   */
+  public void defineBorder(Position position, int dimension, List<int[]> neighbors, Cluster cluster, int[] coordinate) {
+    if (neighbors.size() < 2) { //edge of the board so part of the border of the cluster
+      cluster.coordinatesBorder.add(coordinate);
+    } else {
+      for (int[] neighbor : neighbors) {
+        int index = neighbor[1] * dimension + neighbor[0];
+        // If any of the neighbors is not of the same stone it is a border
+        if (position.getIntersection(index).stone != cluster.stone && index < dimension*dimension
+            && !containsArray(cluster.coordinatesBorder, coordinate)) {
+          cluster.coordinatesBorder.add(coordinate);
+        }
+      }
+    }
+  }
+
+  /**
+   * Find all neighboring intersections, if at the edge of the board there is no neighbor.
+   * Neighbors are all that neighbor the same 4 squares as the current intersection
+   * @param coordinate of the intersection fo which the neighbors are needed
+   * @param dimension of the board
+   * @return the neighbors
+   */
+  public List<int[]> findNeighborsStraigh(int[] coordinate, int dimension) {
+
+    List<int[]> neighbors = new ArrayList<>();
+
+    // Neighbor left
+    if (!Arrays.equals(new int[]{Math.max(coordinate[0] - 1, 0), coordinate[1]},
+        coordinate)) {
+      neighbors.add(new int[]{Math.max(coordinate[0] - 1, 0), coordinate[1]});
+    }
+    // Neighbor right
+    if (!Arrays.equals(new int[]{Math.min(coordinate[0] + 1, dimension - 1), coordinate[1]},
+        coordinate)) {
+      neighbors.add(new int[]{Math.min(coordinate[0] + 1, dimension - 1), coordinate[1]});
+    }
+    // Neighbor up
+    if (!Arrays.equals(new int[]{coordinate[0], Math.max(coordinate[1] - 1, 0)}, coordinate)) {
+      neighbors.add(new int[]{coordinate[0], Math.max(0, coordinate[1] - 1)});
+    }
+    // Neighbor down
+    if (!Arrays.equals(new int[]{coordinate[0], Math.min(dimension - 1, coordinate[1] + 1)},
+        coordinate)) {
+      neighbors.add(new int[]{coordinate[0], Math.min(dimension - 1, coordinate[1] + 1)});
+    }
+    return neighbors;
+  }
+  public List<int[]> findNeighborsDiagnoal(List<int[]> neighbors, int[] coordinate, int dimension) {
+
+    List<int[]> neighborsDiagonal = new ArrayList<>();
+
+    for (int[] neighbor : neighbors) {
+      neighborsDiagonal.add(neighbor.clone());
+    }
+
+    // Neighbor up left (diagonal)
+    if (!Arrays.equals(new int[]{Math.max(0, coordinate[0]-1),  Math.max(0, coordinate[1] - 1)}, coordinate)
+      && !containsArray(neighborsDiagonal, new int[]{Math.max(0, coordinate[0]-1),  Math.max(0, coordinate[1] - 1)})) {
+      neighborsDiagonal.add(new int[]{Math.max(0, coordinate[0]-1), Math.max(0, coordinate[1] - 1)});
+    }
+
+    // Neighbor up right (diagonal)
+    if (!Arrays.equals(new int[]{Math.min(dimension - 1, coordinate[0] + 1), Math.max(0, coordinate[1] - 1)}, coordinate)
+        && !containsArray(neighborsDiagonal, (new int[]{Math.min(dimension - 1, coordinate[0] + 1), Math.max(0, coordinate[1] - 1)}))) {
+      neighborsDiagonal.add(new int[]{Math.min(dimension - 1, coordinate[0] + 1), Math.max(0, coordinate[1] - 1)});
+    }
+
+    // Neighbor down left (diagonal)
+    if (!Arrays.equals(new int[]{Math.max(0, coordinate[0] - 1), Math.min(dimension - 1, coordinate[1] + 1)}, coordinate)
+        && !containsArray(neighborsDiagonal, new int[]{Math.max(0, coordinate[0] - 1), Math.min(dimension - 1, coordinate[1] + 1)})) {
+      neighborsDiagonal.add(new int[]{Math.max(0, coordinate[0] - 1), Math.min(dimension - 1, coordinate[1] + 1)});
+    }
+
+    // Neighbor down right (diagonal)
+    if (!Arrays.equals(new int[]{Math.min(dimension - 1, coordinate[0] + 1), Math.min(dimension - 1, coordinate[1] + 1)}, coordinate)
+        && !containsArray(neighborsDiagonal, new int[]{Math.min(dimension - 1, coordinate[0] + 1), Math.min(dimension - 1, coordinate[1] + 1)})) {
+      neighborsDiagonal.add(new int[]{Math.min(dimension - 1, coordinate[0] + 1), Math.min(dimension - 1, coordinate[1] + 1)});
+    }
+    return neighborsDiagonal;
+  }
+
+  private boolean containsArray(List<int[]> neighbors, int[] target) {
+    for (int[] neighbor : neighbors) {
+      if (Arrays.equals(neighbor, target)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
---
+++ src/test/java/GameTest/Logic_Test.java
@@ -0,0 +1,69 @@
+package GameTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import Connectivity.Server.ConnectionHandler;
+import Game.Board;
+import Game.GoGame;
+import Game.Game;
+import Game.*;
+import com.nedap.go.Go;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Logic_Test {
+  public static final int dimension = 9;
+  public Game goGame;
+  public Board board;
+  public GoLogic logic;
+  public List<ConnectionHandler> players = new ArrayList<>();
+  public ConnectionHandler player1 = new ConnectionHandler();
+  public ConnectionHandler player2 = new ConnectionHandler();
+
+  /**
+   * Before every test create a board and add two players to it.
+   */
+  @BeforeEach
+  void setup() {
+    logic = new GoLogic();
+    players.add(player1);
+    players.add(player2);
+    goGame = new GoGame(dimension, players);
+  }
+
+  @Test
+  void testClusters() {
+    Position newPosition = new Position(dimension);
+    // create a line of black on index 0
+    for (int i = 0; i < dimension; i++) {
+      newPosition.setIntersection(i, Stone.BLACK);
+      newPosition.setIntersection(i+dimension, Stone.WHITE);
+    }
+
+    newPosition.setIntersection(27, Stone.WHITE);
+    newPosition.setIntersection(37, Stone.WHITE);
+
+    newPosition.setIntersection(40, Stone.BLACK);
+    newPosition.setIntersection(41, Stone.BLACK);
+    newPosition.setIntersection(48, Stone.BLACK);
+    newPosition.setIntersection(49, Stone.BLACK);
+    newPosition.setIntersection(50, Stone.BLACK);
+    newPosition.setIntersection(59, Stone.BLACK);
+    newPosition.setIntersection(58, Stone.BLACK);
+
+
+    goGame.setPosition(newPosition);
+    System.out.println(goGame.getStateString());
+    List<Cluster> clusters = logic.stoneClusters(goGame.getStatePosition(), dimension);
+    assertEquals(4,clusters.size());
+    // Check that the borders of each cluster are found correctly
+    assertEquals(clusters.get(0).intersectionList.size(), clusters.get(0).coordinatesBorder.size());
+    assertEquals(clusters.get(1).intersectionList.size(), clusters.get(1).coordinatesBorder.size());
+    assertEquals(clusters.get(2).intersectionList.size(), clusters.get(2).coordinatesBorder.size());
+    // Test cluster with 1 stone not part of the boarder
+    assertEquals(clusters.get(3).intersectionList.size(), clusters.get(3).coordinatesBorder.size()+1);
+  }
+}
