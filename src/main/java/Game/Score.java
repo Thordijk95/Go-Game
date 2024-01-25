ed6eed69ef84cc7220ff06782e8ebf94c2c596dd
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-25 15:50:50 +0100

    Changed naming of fields and toString

--- src/main/java/Game/Score.java
+++ src/main/java/Game/Score.java
@@ -1,11 +1,16 @@
 package Game;
 
 public class Score {
-  public int pointsBlack;
-  public int pointsWhite;
+  public int scoreBlack;
+  public int scoreWhite;
+
+  public Score(int scoreBlack, int scoreWhite) {
+    this.scoreWhite = scoreWhite;
+    this.scoreBlack = scoreBlack;
+  }
 
   public String toString() {
-    return String.format(pointsBlack + "," + pointsWhite);
+    return String.format("Black: " + scoreBlack + "\n" + "White: "+ scoreWhite);
   }
 
 }
