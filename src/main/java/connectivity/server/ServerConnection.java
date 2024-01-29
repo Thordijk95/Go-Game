9fe45afeadf42f2c28680826375edd887e9ad79b
Thomas Hordijk <thomasabelhordijk@gmail.com>
2024-01-29 16:17:24 +0100

    Further implemented the protocol

--- src/main/java/connectivity/server/ServerConnection.java
+++ src/main/java/connectivity/server/ServerConnection.java
@@ -3,6 +3,7 @@ package connectivity.server;
 import connectivity.SocketConnection;
 import connectivity.protocol.GoProtocol;
 
+import game.GoLogic;
 import game.Move;
 import game.Stone;
 import java.io.IOException;
@@ -64,16 +65,28 @@ public class ServerConnection extends SocketConnection {
         // print it to the screen of the current client
         case GoProtocol.LOGIN -> {
           username = substrings[1];
-          connectionHandler.receiveUsername(substrings[1]);;
-        }
-        case GoProtocol.MOVE -> {
-          Stone stone = substrings[2].toLowerCase().equals("Black") ? Stone.BLACK : Stone.WHITE;
-          Move newMove = new Move(stone, Integer.parseInt(substrings[1]));
-          connectionHandler.receiveMove(newMove);
+          connectionHandler.receiveUsername(substrings[1]);
         }
         case GoProtocol.QUEUE -> {
           connectionHandler.receiveQueueRequest();
         }
+        case GoProtocol.MOVE -> {
+          int index;
+          String location = substrings[1];
+          if (location.contains(",")) {
+            String[] coordinates = location.split(",");
+            int column = Integer.parseInt(coordinates[0]);
+            int row = Integer.parseInt(coordinates[1]);
+            index = new GoLogic().calculateIndex(new int[] {column, row});
+          } else {
+            index = Integer.parseInt(location);
+          }
+          Move move = new Move(connectionHandler.stone, index);
+          connectionHandler.receiveMove(move);
+        }
+        case GoProtocol.PASS -> connectionHandler.receivePass();
+        case GoProtocol.RESIGN -> connectionHandler.receiveResign();
+        case GoProtocol.ERROR -> System.out.println(substrings[1]);
         default -> { // invalid entry, do nothing
         }
       }
@@ -87,20 +100,25 @@ public class ServerConnection extends SocketConnection {
   protected void handleDisconnect() {
     if (username == null) {
       username = "no-name";
+    } else {
+      connectionHandler.goServer.removePlayer(connectionHandler);
     }
     System.out.println("Disconnect " + username);
   }
 
   /**
-   * forward the message from the client to the client handler, to the chat server.
-   * @param sender of the message (client)
-   * @param message being sent by the client
+   * Send a move to the players for them to update their board with.
+   * @param protocol used to differentiate different messages
+   * @param move that was played.
    */
-  public void sendMove(String message) {
-    super.sendMessage(String.format(message + ", " + username));
+  public void sendMove(String protocol, String move, Stone stone) {
+    super.sendMessage(String.format(protocol + "~" + move + "~" + stone.toString()));
   }
 
-  public void sendMessage(GoProtocol protocol) { }
+  public void sendMessage(GoProtocol protocol, String message) {
+    String srvrMsg = String.format(protocol + "~" + message);
+    super.sendMessage(srvrMsg);
+  }
 
   public String getUsername() {return username;}
 
